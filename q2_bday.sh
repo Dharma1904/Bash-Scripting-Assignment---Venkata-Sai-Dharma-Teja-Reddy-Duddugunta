fb04336e8f457a421feb5f46d8fa9a7ea755af54
Dharma1904 <dharmatejareddy1904@gmail.com>
2022-03-12 20:05:26 -0500

    Initial commit

---
+++ q2_bday.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BDAY="04/01/1999";
+echo $BDAY;
+
+SUM=0
+num_regex='^[0-9]+$'
+for (( i=0; i<${#BDAY}; i++ )); do
+    if [[ ${BDAY:$i:1} =~ $num_regex ]] ; then
+        SUM=`expr $SUM + ${BDAY:$i:1}`
+    fi
+done
+
+echo $SUM
